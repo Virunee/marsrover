bd2fa88340bf2c11b1a94e0b61e138cc4dc16009
Daria Sirous <daria.sirous@gmail.com>
2017-03-17 12:06:19 -0400

    Add Rover, RoverController with tests

---
+++ lib/rovercontroller.py
@@ -0,0 +1,96 @@
+import utils
+from planetau import Planetau
+from copy import copy
+
+class Rover:
+  def __init__(self, position, direction):
+    """Init Mars Rover with position in Coordinates2D and Direction"""
+    self.__position = None
+    self.__direction = None
+
+    if ( not isinstance(position, utils.Coordinates2D) or
+         direction not in utils.CardinalDirections ):
+      raise ValueError('Check types')
+
+    self.__position = copy(position)
+    self.__direction = copy(direction)
+
+  def __repr__(self):
+    """Return formatted representation string"""
+    return "Mars Rover >> Current position: {0}; Direction: {1}".format(self.__position, self.__direction)
+
+  @property
+  def direction(self):
+    return self.__direction
+
+  @direction.setter
+  def direction(self, newDirection):
+    if newDirection not in utils.CardinalDirections:
+      raise ValueError('Check type of new direction (utils.CardinalDirections)')
+    self.__direction = newDirection
+
+  @property
+  def position(self):
+    return self.__position
+
+  @position.setter
+  def position(self, newPosition):
+    if not isinstance(newPosition, utils.Coordinates2D):
+      raise ValueError('Check type of new position (Coordinates2D)')
+    self.__position = newPosition
+
+
+class RoverController:
+  step = 1
+  def __init__(self, planetau, marsRover = None):
+    """Init Roller Cobtroller with Planetau and MarsRover"""
+    self.__planetau = None
+    self.__marsRover = None
+
+    if not isinstance(planetau, Planetau):
+      raise ValueError('Check planetau')
+    self.planetau = planetau
+
+    if (marsRover and (not isinstance(marsRover, Rover) or
+         marsRover.position not in planetau)):
+      raise ValueError('Chek rover. It could be out of planetau')
+    self.marsRover = marsRover
+
+  def sendCommand(self, command):
+    """Handle commands to move Rover"""
+    if command not in utils.MovingDirections:
+      raise ValueError('Check command (utils.MovingDirections)')
+
+    if command == 'M':
+      self.__move(RoverController.step)
+    else:
+      self.__rotate(command)
+
+  def __rotate(self, direction):
+    """Rotate Rover 90 degrees left or right"""
+    newDirectionIndex = utils.CardinalDirections.index(self.marsRover.direction)
+
+    if (direction == 'R'):
+      newDirectionIndex = (newDirectionIndex-1)%len(utils.CardinalDirections)
+    elif (direction == 'L'):
+      newDirectionIndex = (newDirectionIndex+1)%len(utils.CardinalDirections)
+    
+    self.marsRover.direction = utils.CardinalDirections[newDirectionIndex]
+
+  def __move(self, step):
+    """Move Rover in 1 step according direction"""
+    rawCoordinates = self.marsRover.position
+
+    if (self.marsRover.direction == 'N'):
+      rawCoordinates.y += step
+    elif (self.marsRover.direction == 'E'):
+      rawCoordinates.x += step
+    elif (self.marsRover.direction == 'S'):
+      rawCoordinates.y -= step
+    elif (self.marsRover.direction == 'W'):
+      rawCoordinates.x -= step
+
+    if rawCoordinates not in self.planetau:
+      raise IndexError('Rover can''t go out of bounds of Planetau' )
+
+    self.marsRover.position = rawCoordinates
---
+++ tests/rovercontrollertest.py
@@ -0,0 +1,130 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'lib')))
+
+import unittest
+import utils
+from rovercontroller import Rover, RoverController
+from planetau import Planetau
+
+class TestRover(unittest.TestCase):
+  """Tests for Rover"""
+
+  def test_init_with_valid_input(self):
+    """Check Rover with valid arguments"""
+    direction = "N"
+    point = utils.Coordinates2D(5, 100)
+    rover = Rover(point, direction)
+
+    self.assertEqual(rover.position, point)
+    self.assertEqual(rover.direction, direction)
+
+  def test_init_with_invalid_input(self):
+    """Check Rover with invalid arguments"""
+    self.assertRaises(ValueError, Rover, "N", "N")
+    self.assertRaises(ValueError, Rover, utils.Coordinates2D(5,100), "K")
+    self.assertRaises(ValueError, Rover, "N", "K")
+
+  def test_properties(self):
+    """Check setters of properties"""
+    rover = Rover(utils.Coordinates2D(5, 100), "N")
+    self.assertRaises(ValueError, setattr, rover, "position", "position")
+    self.assertRaises(ValueError, setattr, rover, "position", 100)
+    self.assertRaises(ValueError, setattr, rover, "direction", "K")
+    self.assertRaises(ValueError, setattr, rover, "direction", 100)
+
+
+
+class TestRoverController(unittest.TestCase):
+  """Tests for RoverController"""
+  
+  def setUp(self):
+    """Setup parameters to test with"""
+    self._point = utils.Coordinates2D(5, 100)
+    self._direction = "N"
+    self._planetau = Planetau(self._point)
+    self._rover = Rover(self._point, self._direction)
+
+
+  def test_init_with_valid_input(self):
+    """Check RoverController with valid arguments"""
+    rc = RoverController(self._planetau, self._rover)
+    rc2 = RoverController(self._planetau)
+
+    self.assertEqual(rc.planetau, self._planetau)
+    self.assertEqual(rc.marsRover.position, self._point)
+    self.assertEqual(rc.marsRover.direction, self._direction)
+
+    self.assertEqual(rc2.planetau, self._planetau)
+
+
+  def test_init_with_invalid_input(self):
+    """Check RoverController with point out of planetau"""
+    rover = Rover(utils.Coordinates2D(100, 100), self._direction)
+    self.assertRaises(ValueError, RoverController, self._planetau, rover)
+
+
+  def test_send_invalid_command(self):
+    """Chek send invalid command to Rover"""
+    point = utils.Coordinates2D(5, 100)
+    rc = RoverController(Planetau(point), Rover(point, "N"))
+
+    self.assertRaises(ValueError, rc.sendCommand, "X")
+    self.assertRaises(ValueError, rc.sendCommand, 500)
+
+
+  def test_send_command_rotate_left(self):
+    """Check send commands to rover: N->E->S->W"""
+    rc = RoverController(self._planetau, self._rover)
+
+    rc.sendCommand("L")
+    self.assertEqual(rc.marsRover.direction, "E")
+    rc.sendCommand("L")
+    self.assertEqual(rc.marsRover.direction, "S")
+    rc.sendCommand("L")
+    self.assertEqual(rc.marsRover.direction, "W")
+    rc.sendCommand("L")
+    self.assertEqual(rc.marsRover.direction, "N")
+
+
+  def test_send_command_rotate_right(self):
+    """Check send commands to rover: N->W->S->E"""
+    rc = RoverController(self._planetau, self._rover)
+
+    rc.sendCommand("R")
+    self.assertEqual(rc.marsRover.direction, "W")
+    rc.sendCommand("R")
+    self.assertEqual(rc.marsRover.direction, "S")
+    rc.sendCommand("R")
+    self.assertEqual(rc.marsRover.direction, "E")
+    rc.sendCommand("R")
+    self.assertEqual(rc.marsRover.direction, "N")
+
+
+  def test_send_command_move_valid(self):
+    """Check movind on 1 step according to direction"""
+    rc = RoverController(self._planetau, self._rover)
+
+    rc.sendCommand("R")
+    rc.sendCommand("M")
+    self.assertEqual(rc.marsRover.position, utils.Coordinates2D(4, 100))
+    rc.sendCommand("R")
+    rc.sendCommand("M")
+    self.assertEqual(rc.marsRover.position, utils.Coordinates2D(4, 99))
+    rc.sendCommand("R")
+    rc.sendCommand("M")
+    self.assertEqual(rc.marsRover.position, utils.Coordinates2D(5, 99))
+    rc.sendCommand("R")
+    rc.sendCommand("M")
+    self.assertEqual(rc.marsRover.position, utils.Coordinates2D(5, 100))
+
+  def test_send_command_move_invalid(self):
+    """Check movind out of planetau bounds"""
+    rc = RoverController(self._planetau, self._rover)
+
+    rc.sendCommand("L")
+    rc.sendCommand("R")
+    self.assertRaises(IndexError, rc.sendCommand, "M")
+
+
+if __name__ == '__main__':
+    unittest.main()
